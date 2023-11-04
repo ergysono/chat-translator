af96ad845eece3c82b5900da4395a8ab9cce2be4
Ergys <gysigba@gmail.com>
2023-11-04 13:42:16 +0100

    added header with shadcn components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
+import DarkModeToggle from "./DarkModeToggle";
+import UserButton from "./UserButton";
 import Logo from "./Logo";
 
 function Header() {
-  return <header>
-    <nav>
-        <Logo></Logo>
-    </nav>
-  </header>;
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          <DarkModeToggle />
+          <UserButton />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
