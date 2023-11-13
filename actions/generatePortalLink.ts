b960921a11e3bf5d40d13d0861c903ce794d29f0
Ergys <gysigba@gmail.com>
2023-11-13 19:56:47 +0100

    fixed billing portal

--- actions/generatePortalLink.ts
+++ actions/generatePortalLink.ts
@@ -12,8 +12,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export async function generatePortalLink() {
-  console.log("hiiii");
-  debugger;
   const session = await getServerSession(authOptions);
   const host = headers().get("host");
 
@@ -21,21 +19,19 @@ export async function generatePortalLink() {
     console.error("No user id found");
   }
 
+  const {
+    user: {id},
+  } = session!;
+
   const returnUrl =
     process.env.NODE_ENV === "development"
       ? `http://${host}/register`
       : `https://${host}/register`;
 
-  const doc = await adminDb
-    .collection("customers")
-    .doc("Z1YffJiEsRr06DlwZ9Xu")
-    .get();
+  const doc = await adminDb.collection("customers").doc(id).get();
 
   if (!doc.data) {
-    return console.error(
-      "No customer record found with userId: ",
-      "Z1YffJiEsRr06DlwZ9Xu"
-    );
+    return console.error("No customer record found with userId: ", id);
   }
 
   const stripeId = doc.data()!.stripeId;
